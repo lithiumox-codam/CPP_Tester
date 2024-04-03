6061688798adf64742d13eea9cbca37a57eb034c
meesdekker.xyz <me@meesdekker.xyz>
2024-04-03 14:58:19 +0200

    added an extra test for megaphone

--- tests/00ex00.test.ts
+++ tests/00ex00.test.ts
@@ -2,25 +2,31 @@ import { expect, test, describe } from "bun:test";
 import { $ } from "bun";
 import { test_dir } from "..";
 
-const program = `${test_dir}/00/ex00/megaphone`
+const program = `${test_dir}/00/ex00/megaphone`;
 
 describe("00/ex00", () => {
     test("no args", async () => {
-        const res = await $`${program}`.text()
-        expect(res).toBe("* LOUD AND UNBEARABLE FEEDBACK NOISE *")
+        const res = await $`${program}`.text();
+        expect(res).toBe("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
     });
     test("hello world", async () => {
-        const res = await $`${program} "hello world"`.text()
-        expect(res).toBe("HELLO WORLD\n")
+        const res = await $`${program} "hello world"`.text();
+        expect(res).toBe("HELLO WORLD\n");
     });
     test("chars (alphanumeric)", async () => {
-        const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        const res = await $`${program} ${chars}`.text()
-        expect(res).toBe(chars.toUpperCase() + "\n")
+        const chars =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        const res = await $`${program} ${chars}`.text();
+        expect(res).toBe(chars.toUpperCase() + "\n");
     });
     test("chars (non-alphanumeric)", async () => {
-        const chars = "!@#$%^&*()_+{}[]|\\;':\",./<>?"
-        const res = await $`${program} ${chars}`.text()
-        expect(res).toBe(chars + "\n")
+        const chars = "!@#$%^&*()_+{}[]|\\;':\",./<>?";
+        const res = await $`${program} ${chars}`.text();
+        expect(res).toBe(chars + "\n");
+    });
+    test("mixed", async () => {
+        const chars = "hello world!@#$%^&*()_+{}[]|\\;':\",./<>?";
+        const res = await $`${program} ${chars}`.text();
+        expect(res).toBe("HELLO WORLD!@#$%^&*()_+{}[]|\\;':\",./<>?\n");
     });
 });
